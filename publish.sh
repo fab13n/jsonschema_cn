f88f149239d9b01c49cb35422ebfda7319311034
Fleutot <fleutot@gmail.com>
2019-11-12 10:36:02 +0100

    cleanup

--- publish.sh
+++ publish.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
 VERSION=$1
+
+cd $(dirname $0)
+
+# Check if version has been provided
 if [[ "$VERSION" == "" ]]; then
     CURRENT_VERSION=$(cat jsonschema_cn/__init__.py | grep '^\s*__version__\s*=' | cut -d'"' -f2)
     echo "Usage: $0 <new_version number>. Current version is $CURRENT_VERSION."
@@ -11,16 +16,21 @@ if [[ "$VERSION" == "" ]]; then
     exit 1
 fi
 
+# Change version in sources
 sed -i "s/^__version__\\s*=.*/__version__ = \"$VERSION\"/" jsonschema_cn/__init__.py
 
+# Commit + tag + push change(s)
 git commit -am "Published version $VERSION"
 git tag -a "v$VERSION" -m "Published version $VERSION"
 git push
 
-python3 setup.py sdist bdist_wheel && \
-. .credentials && \
+# Rebuild
+python3 setup.py sdist bdist_wheel || exit 1
+
+# Publish
+. .credentials
 twine upload \
       --username "$LOGIN" \
       --password "$PASSWORD" \
       --repository-url https://upload.pypi.org/legacy/ \
-      dist/*
+      dist/*-$VERSION.*
