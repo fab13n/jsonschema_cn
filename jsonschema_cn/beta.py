c732d3761fc6ed1ba57644ea9a16a51140c66d91
Fleutot <fleutot@gmail.com>
2019-11-13 15:33:20 +0100

    removed support for nested schema+definitions

--- jsonschema_cn/beta.py
+++ jsonschema_cn/beta.py
@@ -2,22 +2,10 @@ from . import tree as T
 
 class BetaReductionVisitor(object):
 
-    def __init__(self):
-        self.definitions = None
-        self.saved_definitions = []
-
     def visit_Schema_down(self, t: T.Schema) -> None:
-        self.saved_definitions.append(self.definitions)
-        if self.definitions is None:
-            self.definitions = t.definitions
-        else:
-            merged_defs = {}
-            merged_defs.update(self.definitions.values)
-            merged_defs.update(t.definitions.values)
-            self.definitions = T.Defitions(values=merged_defs)
+        self.definitions = t.definitions
 
     def visit_Schema_up(self, t: T.Schema) -> T.Schema:
-        self.definitions = self.saved_definitions.pop()
         return T.Schema(value=t.value, definitions=T.Definitions(values={}))
 
     def visit_Reference_down(self, t: T.Reference) -> T.Type:
