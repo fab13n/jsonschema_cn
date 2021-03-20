c95913784df891d80f7fdea6eb564f75302f761b
Fabien Fleutot <fabien.fleutot@gmail.com>
2021-03-20 15:17:34 +0100

    update test script

--- test.sh
+++ test.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-ENV=venvpy36
-if [[ ! -d "$ENV" ]]; then
-    python3 -m venv $ENV
-    ln -s venvpy36/bin/activate .
+ENV=venv
+ENV_ABS=$(cd $(dirname $0)/$ENV && pwd)
+if [[ ! -d "$ENV_ABS" ]]; then
+    # python3 -m venv $ENV
+    ln -s $ENV_ABS/bin/activate .
     . activate
-    pip install jsview jsonschema parsimonious ipython coverage wheel
-    python setup.py bdist_wheel 
+    $ENV_ABS/bin/pip install jsview jsonschema parsimonious ipython coverage wheel
+    $ENV_ABS/bin/python setup.py bdist_wheel 
 fi
 if [[ "$VIRTUAL_ENV" == "" ]]; then
-    . activate
+    . $ENV_ABS/bin/activate
 fi
-coverage erase
-coverage run --include './jsonschema_cn/**' -m jsonschema_cn.test
-coverage report -m
+COVERAGE=$ENV_ABS/bin/coverage
+$COVERAGE erase
+$COVERAGE run --include './jsonschema_cn/**' -m jsonschema_cn.test
+$COVERAGE report -m
